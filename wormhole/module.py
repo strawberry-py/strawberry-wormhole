ed12cc14dc59d8a66a580c62ca609303f008beed
MatejMartinek <martim28@DESKTOP-MATEJ-LENOVO.localdomain>
2025-08-02 23:03:32 +0200

    black check

--- wormhole/module.py
+++ wormhole/module.py
@@ -8,7 +8,6 @@ from discord.ext import commands, tasks
 
 from pie import check, i18n, logger, storage, utils
 from pie.bot import Strawberry
-
 from .database import (  # Local database model for managing wormhole channels
     WormholeChannel,
 )
@@ -94,13 +93,11 @@ class Wormhole(commands.Cog):
         )
 
         if message.reference and message.reference.type == MessageReferenceType.reply:
-            formatted_message = (
-                f"> {message.reference.cached_message.content.replace("\n","\n> ") if message.reference.cached_message else "Unknown"}\n{formatted_message}"
-            )
-        elif message.reference and message.reference.type == MessageReferenceType.forward:
-            formatted_message = (
-                f"**{guild_display} {message.author.name}:** Forwarded\n```{message.reference.cached_message.content if message.reference.cached_message else "Unknown forwarded message"}```"
-            )
+            formatted_message = f"> {message.reference.cached_message.content.replace("\n", "\n> ") if message.reference.cached_message else "Unknown"}\n{formatted_message}"
+        elif (
+                message.reference and message.reference.type == MessageReferenceType.forward
+        ):
+            formatted_message = f"**{guild_display} {message.author.name}:** Forwarded\n```{message.reference.cached_message.content if message.reference.cached_message else "Unknown forwarded message"}```"
         return formatted_message
 
     async def _set_slowmode(
